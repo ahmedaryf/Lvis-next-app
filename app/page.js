27accd2171f8c6d5ba62bb1f913b254c9d93ceb4
ahmedaryf <ahmed.areef@hotmail.com>
2023-08-20 17:30:12 +0500

    mobile view a photo added instead of a video

--- app/page.js
+++ app/page.js
@@ -1,45 +1,55 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PageRapper from "./components/PageRapper";
 import ReactPlayer from 'react-player';
-// import manta from '../public/videos/mantaWeb-m.mp4';
-
+import picture from '../public/images/ocean-night1.jpg'
+import Image from 'next/image';
 
 export default function Home() {
-  const [playing, setPlaying] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const handlePlay = () => {
-    setPlaying(true);
-  };
+  useEffect(() => {
+    // Update the isMobile state based on window width
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <PageRapper>
       <main className="min-h-screen bg-gradient-to-r from-cyan-300 to-blue-500 dark:from-cyan-800 dark:to-blue-900">
-        <div className=" w-screen overflow-hidden md:h-[80vh] relative">
-
-         <ReactPlayer
-            playing={playing}
-            muted={true}
-            loop={true}
-            url="https://res.cloudinary.com/db95xjjk2/video/upload/v1692417072/mantaWeb-m_c5uzzh.mp4"
-            width="100%"
-            height="auto"
-            className="w-screen h-auto"
-          />
-          {!playing && (
-            <div className="absolute top-0 left-0 w-full h-full" onClick={handlePlay}>
-              Play
-            </div>
+        <div className="w-screen overflow-hidden md:h-[80vh] relative">
+          {isMobile ? (
+            <Image src={picture} alt='Image' />
+          ) : (
+            <ReactPlayer
+              playing={true}
+              muted={true}
+              loop={true}
+              url="https://res.cloudinary.com/db95xjjk2/video/upload/v1692417072/mantaWeb-m_c5uzzh.mp4"
+              width="100%"
+              height="auto"
+              className="w-screen h-auto"
+            />
           )}
 
-          <div className=" bg-black/20 py-2 w-full absolute top-[75%] md:top-[85%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-            <h2 className="text-5xl md:text-8xl font-medium md:font-bold text-white text-center [text-shadow:_4px_1px_2px_rgb(0_0_0_/_60%)] ">LVIS Hotels</h2>
-            <h1 className="text-center text-white md:text-2xl">Accommodation for visitors of Hanifaru bay</h1>
+          <div className="bg-black/20 py-2 w-full absolute top-[75%] md:top-[85%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h2 className="text-5xl md:text-8xl font-medium md:font-bold text-white text-center [text-shadow:_4px_1px_2px_rgb(0_0_0_/_60%)] ">
+              LVIS Hotels
+            </h2>
+            <h1 className="text-center text-white md:text-2xl">
+              Accommodation for visitors of Hanifaru bay
+            </h1>
           </div>
         </div>
-        <div>
-
-        </div>
+        <div></div>
       </main>
     </PageRapper>
   );
